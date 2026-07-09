bd27e5598e261a712a54163023d513f885f47172
Jimin Hu <hujimin@example.com>
2026-07-09 15:38:25 -0700

    fix typo

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -95,5 +95,5 @@ REST_FRAMEWORK = {
     ]
 }
 
-CELERY_BROKER_URL=redis://localhost:6379/0
-CELERY_BROKER_URL=redis://localhost:6379
+CELERY_BROKER_URL= "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND="redis://localhost:6379/0"
