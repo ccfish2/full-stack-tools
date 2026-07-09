d9a8d365d4f02f4876c885255633f2b02c793da6
Jimin Hu <hujimin@example.com>
2026-07-09 12:19:26 -0700

    api/newdata/event fix

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,16 +1,7 @@
 from rest_framework import serializers
-from models import StatsigApplication
+from .models import StatsigApplication
 
-class StatsigSerializer(serializers.Serializer):
-    data = serializers.JSONField(max=5000)
-    created_at = serializers.DateTimeField()
-    updated_at = serializers.DateTimeField()
-
-    def create(self, validated_data):
-        return StatsigApplication(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.data = validated_data.get('data', instance.email)
-        instance.created_at = validated_data.get('created_at', instance.content)
-        instance.updated_at = validated_data.get('updated_at', instance.created)
-        return instance
+class StatsigSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StatsigApplication
+        fields = ['id', 'data', 'updated_at', 'created_at']
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,38 +1,10 @@
-from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
-from serializers import StatsigSerializer
+
 from rest_framework import viewsets
-from rest_framework.response import Response
-from .models import StatsigApplication
 
-def api_hello(request):
-    return JsonResponse({
-        "message": "Hello from Django backend",
-        "status": "ok",
-    })
+from backend.core.serializers import StatsigSerializer
 
-class StatsigViewSet(viewsets.ViewSet):
-    """
-      mainly post
-    """
-
-    def list(self, request):
-        pass
-
-    def create(self, request):
-        pass
-
-    def retrieve(self, request, pk=None):
-        pass
-
-    def update(self, request, pk=None):
-        pass
-
-    def partial_update(self, request, pk=None):
-        pass
-
-    def destroy(self, request, pk=None):
-        pass
+class StatsigViewSet(viewsets.ModelViewSet):
+    serializer_class = StatsigSerializer
+   
+    def get_queryset(self):
+        return
