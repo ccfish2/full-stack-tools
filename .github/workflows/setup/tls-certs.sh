047468af3fdf3a99a1d3924266d7911b3eb1b2a0
Jimin Hu <hujimin@example.com>
2026-08-04 23:16:01 -0700

    secure service use https and gateway terminate TLS

--- .github/workflows/setup/tls-certs.sh
+++
@@ -1,5 +0,0 @@
-# svc.ns.local
-# no public FQDN
-./mkcert fullstack.dolphin.local 127.0.0.1
-k -n dolphin create secret tls fullstack-tls --cert fullstack.dolphin.local+1.pem --key fullstack.dolphin.local+1-key.pem
-curl -v --cacert fullstack.dolphin.local+1.pem  --resolve fullstack.dolphin.local:443:{gateway-external-IP}  https://fullstack.dolphin.local/api/v1/hello -L
